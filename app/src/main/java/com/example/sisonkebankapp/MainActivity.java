14cf0eb12011d9b8eb11437708b9bed92bcd542f
Synx-x <siyandamtamo@gmail.com>
2020-09-25 13:26:13 +0200

    Finished main 3 activities, not tested

--- app/src/main/java/com/example/sisonkebankapp/MainActivity.java
+++ app/src/main/java/com/example/sisonkebankapp/MainActivity.java
@@ -8,12 +8,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText uiEmail;
     private EditText uiPassword;
     private Button uiLogin;
     private TextView uiRegister;
+    boolean isValid = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +28,54 @@ public class MainActivity extends AppCompatActivity {
         uiLogin = findViewById(R.id.login);
         uiRegister = findViewById(R.id.register);
 
-        //button redirects to the registration activity if user doesnt have account
+        //button redirects to the registration activity if user doesn't have account
         uiRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
             }
         });
+
+        uiLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //getting user input from the 2 input fields
+                String inputName = uiEmail.getText().toString();
+                String inputPassword = uiPassword.getText().toString();
+
+                //validating inputs provided by a user
+                if(inputName.isEmpty() || inputPassword.isEmpty())
+                {
+                    Toast.makeText(MainActivity.this, "Please enter all your details correctly.", Toast.LENGTH_SHORT).show();
+                }else {
+
+                    isValid = validateLogin(inputName, inputPassword);
+                    if(!isValid){
+                        Toast.makeText(MainActivity.this, "Incorrect Login Details", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+
+                        //once login is successful, this intent redirects to the main page activity
+
+                        //create intent
+                        Intent intent = new Intent(MainActivity.this, MainPageActivity.class);
+
+                        //start intent
+                        startActivity(intent);
+
+                    }
+
+                }
+            }
+        });
     }
+    //validate credentials on database
+    private boolean validateLogin(String email, String password){
+        if(email.equalsIgnoreCase(RegistrationActivity.credentials.getDbEmail()) && password.equalsIgnoreCase(RegistrationActivity.credentials.getDbPassword())){
+            return true;
+        }
+        return false;
+    }
+
 }
